696e303ffd1e99133d1a30ee570f70d8682830e6
fanqi <me@fanqi.xyz>
2017-08-25 17:43:26 +0800

    改变部署分支 wedeploy 为 gh-pages

--- electric.config.js
+++ electric.config.js
@@ -7,5 +7,6 @@ module.exports = {
 	sassOptions: {
 		includePaths: ['node_modules', marble.src]
 	},
-	vendorSrc: ['node_modules/marble/build/fonts/**']
+	vendorSrc: ['node_modules/marble/build/fonts/**'],
+	deployOptions: { branch: 'gh-pages' }
 };
